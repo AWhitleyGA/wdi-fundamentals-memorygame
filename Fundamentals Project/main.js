25415e1b52141e9d9faaef95fdfa6980a6ea01c4
Andrew Whitley <awhitley1233@gmail.com>
2016-09-01 19:08:43 -0400

    added JS functions for creating cards on board/created firing button

--- Fundamentals Project/main.js
+++ Fundamentals Project/main.js
@@ -3,8 +3,21 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
 
-if (cardOne === cardThree) {
-  alert("You found a match");
-} else {
-  alert("Sorry, try again");
+// if (cardOne === cardThree) {
+//   alert("You found a match");
+// } else {
+//   alert("Sorry, try again");
+// }
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+  for (i = 0; i <= 3; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    gameBoard.appendChild(newCard);
+  }
 }
+
+var button = document.getElementById('button');
+button.addEventListener('click', createBoard);
