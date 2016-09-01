ee850b910cfd5b31b843d61d2e68de8acdc944af
Andrew Whitley <awhitley1233@gmail.com>
2016-09-01 13:22:31 -0400

    added conditional statement to main.js

--- Fundamentals Project/main.js
+++ Fundamentals Project/main.js
@@ -2,3 +2,9 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardOne === cardThree) {
+  alert("You found a match");
+} else {
+  alert("Sorry, try again");
+}
